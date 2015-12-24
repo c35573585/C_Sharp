b6c2609d68a873c5c546a74d0654244f4c351d0a
c35573585 <c35573585@yahoo.com.tw>
2015-12-24 14:19:03 +0800

    Class_Part3
    Inheritance--virtual--override--abstract--sealed

---
+++ Learning in MVA/UnderstandingInheritance/UnderstandingInheritance/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "BMW";
+            myCar.Model = "745li";
+            myCar.Year = 2005;
+            myCar.Color = "Black";
+            printVehicleDetails(myCar);
+
+            Truck myTruck = new Truck();
+            myTruck.Make = "Ford";
+            myTruck.Model = "F950";
+            myTruck.Year = 2006;
+            myTruck.Color = "Black";
+            myTruck.TowingCapacity = 1200;
+            printVehicleDetails(myTruck);
+
+            Console.ReadLine();
+        }
+        private static void printVehicleDetails(Vehicle vehicle)
+        {
+            Console.WriteLine("Here are the vehicle's details: {0}",
+                vehicle.FormatMe());
+        }
+    }
+
+    abstract class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public abstract string FormatMe();
+    }
+
+    class Car : Vehicle
+    {
+        public override string FormatMe()
+        {
+            return String.Format("{0} - {1} - {2} - {3}",
+                this.Make,
+                this.Model,
+                this.Year,
+                this.Color);
+        }
+    }
+
+    sealed class Truck : Vehicle//Anyone can't inheritance from here
+    {
+        public int TowingCapacity { get; set; }
+
+        public override string FormatMe()
+        {
+            return String.Format("{0} - {1} - {2} Towings Units",
+                this.Make,
+                this.Model,
+                this.TowingCapacity);
+        }
+    }
+}
