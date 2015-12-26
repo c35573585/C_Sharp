03f7a5e1568491f15c340b7ffc633af924de5785
c35573585 <c35573585@yahoo.com.tw>
2015-12-26 13:29:49 +0800

    Enumerations
    Enumerations

---
+++ Learning in MVA/UnderstandingEnumerations/UnderstandingEnumerations/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingEnumerations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine("Type in a SuperHero's name to see his nickname:");
+            string userValue = Console.ReadLine();
+
+            SuperHero myValue;
+
+            if (Enum.TryParse<SuperHero>(userValue, true, out myValue))
+            {
+                switch (myValue)
+                {
+                    case SuperHero.Batman:
+                        Console.WriteLine("Caped Crusader");
+                        break;
+                    case SuperHero.Superman:
+                        Console.WriteLine("Man of Steel");
+                        break;
+                    case SuperHero.GreenLantern:
+                        Console.WriteLine("Emerald Knight");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Does not copute!");
+            }
+
+            //Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+
+    enum SuperHero
+    {
+        Batman,
+        Superman,
+        GreenLantern
+    }
+}
