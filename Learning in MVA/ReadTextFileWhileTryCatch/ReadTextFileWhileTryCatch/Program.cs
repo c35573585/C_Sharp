6e1ee5f326f2f794a9c5be7557add77fc9979607
c35573585 <c35573585@yahoo.com.tw>
2015-12-26 13:29:21 +0800

    Try_Catch
    Try_Catch exception

---
+++ Learning in MVA/ReadTextFileWhileTryCatch/ReadTextFileWhileTryCatch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadTextFileWhileTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader myReader = new StreamReader("Values.txt");
+                string line = "";
+
+                while (line != null)
+                {
+                    line = myReader.ReadLine();
+                    if (line != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                myReader.Close();
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("Couldn't find the file. Are you sure the DIREACTORY exists?");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Couldn't find the file. Are you sure you're looking for the correct file?");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something didn't quite work correctly: {0}", e.Message);
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
