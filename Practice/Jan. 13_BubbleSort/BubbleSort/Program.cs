5673ca61fc5ee404421e3fccf94b163fd69d9c5a
c35573585 <c35573585@yahoo.com.tw>
2016-01-14 14:52:37 +0800

    Bubble Sort
    Bubble Sort

---
+++ Practice/Jan. 13_BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bubble();
+            Console.ReadLine();
+        }
+        public static void Bubble()
+        {
+            int[] array = new int[]{2, 5, 1, 4, 3};
+            
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = 0;
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;   
+                    }
+                }
+            }
+
+            foreach (int a in array)
+            {
+                Console.Write(a + " ");
+            }
+
+        }
+    }
+
+    
+}
